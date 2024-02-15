c5594dcbc3699135347ed8c400d3eeb905d67e83
sarapiraquive <sarapirpa@unisabana.edu.co>
2024-02-15 11:13:54 -0500

    Crear clase ComandoTareaCompletada que implementa la interfaz Comando y se encarga de completar tarea especifica

---
+++ src/gestionTareas/ComandoTareaCompletada.java
@@ -0,0 +1,16 @@
+package gestionTareas;
+
+public class ComandoTareaCompletada implements Comando{
+    private Tarea tarea;
+
+    public ComandoTareaCompletada(Tarea tarea) {
+        this.tarea = tarea;
+    }
+
+    public void execute() {
+        tarea.completo();
+    }
+
+    public void undo() {
+        tarea.DesmarcarTareaCompleta();
+}
